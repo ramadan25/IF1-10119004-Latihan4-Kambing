9b90104a8d6286f6c5e438b57cb0188efdcd200b
ramadan25 <72897383+ramadan25@users.noreply.github.com>
2020-10-20 11:18:00 +0700

    Add files via upload

---
+++ IF1-10119004-Latihan4-Kambing/src/if1/pkg10119004/latihan4/kambing/IF110119004Latihan4Kambing.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119004.latihan4.kambing;
+
+/**
+ *
+ * @author ENDOG
+ * NAMA     : Muhamad Ramadan
+ * KELAS    : IF 1
+ * NIM      : 10119004
+ * Deskripsi Program : Program ini berisi untuk menampilkan
+ * value ke layar
+ */
+public class IF110119004Latihan4Kambing {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public class Kambing {
+        
+        public void tambahKambing() {
+            // Deklarasi variabel lokal
+            int jumlahKambing = 0;
+            
+            jumlahKambing = jumlahKambing + 5;
+            System.out.println("Jumlah Kambing setelah ditambah:" + jumlahKambing);
+        }
+    }
+    public static void main(String[] args) {
+        Kambing = new Kambing();
+    }
+    
+}
